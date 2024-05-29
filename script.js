4b205ceab2d47a1cc8197e4e5789de6348c6c3cf
Thiago Bertolino <thiagopbertolino@gmail.com>
2024-05-29 15:49:46 -0300

    UP

---
+++ script.js
@@ -0,0 +1,8 @@
+// Adicionando funcionalidade para o menu hamburger
+
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+});
